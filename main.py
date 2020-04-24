75e14c39795979137bb0ccab5a4a27aa9b131b8c
Anca <joker103186@gmail.com>
2020-04-24 11:39:32 +0800

    update main.py
    start bot in discord

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello')
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_preFix = '[')
+
+@bot.event
+async def on_ready():
+    print("Bot works successfully!")
+
+bot.run('NzAzMDc0ODg3NjQ3ODg3NDYx.XqJTog.do9Z6VgFEun_7u0KNhL7Uc933x0')
