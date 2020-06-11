17de406aba1e40f4f0ef8bf003b2ae0aabe37ed2
Ancacion <joker103186@gmail.com>
2020-06-11 00:36:45 -0700

    0611 3rd update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix='!')
 bot.load_extension('code.cmds') 
 bot.load_extension('code.showPicGif')
 bot.load_extension('code.listener')
-#bot.load_extension('code.fastcar')
+bot.load_extension('code.fastcar')
 
 #當bot成功上線時輸出成功訊息
 @bot.event
@@ -33,39 +33,6 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(data['IN_SERVER & OUT_SERVER'])
     await channel.send(f'{member} 離開我們了，我們懷念他!')
-
-#catch the webside of NTUT
-@bot.command()
-async def NTUT(ctx, *, arg):
-    url = "https://www.ntut.edu.tw/"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
-
-    inputstring = arg
-    x = inputstring.split()
-    await ctx.send(x)
-    
-    if x[0] == "id":
-        html = soup.find(id = x[1]).text
-        await ctx.send(len(html))
-        if len(html) > 2000:
-            await ctx.send("This html is too long that can't output.\n")
-        else:
-            await ctx.send(html)
-
-    elif x[0] == "class":
-        html = soup.find(class_ = x[1]).text
-        if len(html) > 2000:
-            await ctx.send("This html is too long that can't output.\n")
-        else:
-            await ctx.send(html)
-
-#catch the webside of PIXIV
-@bot.command()
-async def Pixiv(ctx, arg):
-    url = "https://www.pixiv.net/"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
 
 if __name__ == "__main__":
     bot.run(data['DISCORD_TOKEN'])
