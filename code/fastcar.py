b30a7df432f68639278ffbdb4ba31969077e48a9
Ancacion <joker103186@gmail.com>
2020-06-15 01:38:49 -0700

    IBOT 0615 2nd update

--- code/fastcar.py
+++ code/fastcar.py
@@ -16,31 +16,6 @@ class fastcar(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def NTUT(self, ctx, *, arg):
-        url = data['NTUT']
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "lxml")
-
-        inputstring = arg
-        x = inputstring.split()
-        await ctx.send(x)
-    
-        if x[0] == "id":
-            html = soup.find(id = x[1]).text
-            await ctx.send(len(html))
-            if len(html) > 2000:
-                await ctx.send("This html is too long that can't output.\n")
-            else:
-                await ctx.send(html)
-
-        elif x[0] == "class":
-            html = soup.find(class_ = x[1]).text
-            if len(html) > 2000:
-                await ctx.send("This html is too long that can't output.\n")
-            else:
-                await ctx.send(html)
-
-    @commands.command()
     async def Car(self, ctx, arg):
         if arg.isdigit():
             if len(arg) == 8:
