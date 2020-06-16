ca0578f2ba7772a6ab2646b4c3a9c1c6018bb3e2
IBOTIAndy <visua64@gmail.com>
2020-06-15 23:19:27 -0700

    add command helps & sub

--- code/cmds.py
+++ code/cmds.py
@@ -5,6 +5,7 @@ import random
 import os
 import json
 import time
+#This is use vanilla command.
 
 with open('set.json', 'r', encoding = 'utf8') as jfile:
     data = json.load(jfile)
@@ -12,10 +13,26 @@ with open('set.json', 'r', encoding = 'utf8') as jfile:
 class cmds(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+#ctx = discord bot
+#await = ?
+    @commands.command()
+    async def helps(self, ctx):
+        await ctx.send("we own these order:\n")
+        await ctx.send("!add [number1] [number2]")
+        await ctx.send("!sub [number1] [number2]")
+        await ctx.send("!ping")
+        await ctx.send("!pan")
+        await ctx.send("!USSR")
+        await ctx.send("!USSRExtreme")
+        await ctx.send("!PS5")
 
     @commands.command()
     async def add(self, ctx, a: int, b: int):
         await ctx.send(a + b)
+    
+    @commands.command()
+    async def sub(self, ctx, a: int, b: int):
+        await ctx.send(a - b)
 
     @commands.command()
     async def ping(self, ctx):
