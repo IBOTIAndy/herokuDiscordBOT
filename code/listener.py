719d2963f338bede49fa03d82e05281086b99fc9
Ancacion <joker103186@gmail.com>
2021-05-10 02:22:42 -0700

    Update Marxist and listener

--- code/listener.py
+++ code/listener.py
@@ -30,13 +30,10 @@ class listener(commands.Cog):
         #elif message.content == "test":
         #    await message.channel.send('Message from {0.author}: {0.content}\n'.format(message))
 
-        elif ("學習馬克思" in message.content) or ("成為社會主義" in message.content):
-            await message.channel.send('立即學習馬克思主義，成為社會主義好青年!\n data['Marxist'] \n'.format(message))
-
-        elif message.author != "IBOTIBOT#1854" and "我們" in message.content:
+        elif message.author != "IBOTIAndy#1854" and "我們" in message.content:
             pass
 
-        elif message.author != "IBOTIBOT#1854" and "我" in message.content:
+        elif message.author != "IBOTIAndy#1854" and "我" in message.content:
             await message.channel.send("{0.author}同志。不單只是我，是「我們」\n".format(message))
 
         elif message.author != "Наш робот Discord#8637" and "盤" in message.content:
